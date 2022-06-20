59d27b09037566301768e297f4c303cb96ef1e64
kleisinger <mathew.kleisinger@gmail.com>
2022-06-20 13:19:26 -0500

    style modules added

--- src/components/ProviderList.jsx
+++ src/components/ProviderList.jsx
@@ -1,15 +1,21 @@
 import Provider from "./Provider";
+import styles from "./ProviderList.module.css";
 
 const ProviderList = ({providers, watchList, toggle}) => {
    
     return (  
-    <div className="titleList"> 
-        <div className="title"> 
+    <div className={styles.titleList}> 
+        <div className={styles.title}> 
             <h1>Providers</h1> 
-            <div className="titles-wrapper">
+            <div className={styles.titlesWrapper}>
                 {providers === null ? <p>No Results Found</p> :
                 providers.map((provider) =>
-                    <Provider key={provider.provider_id} provider={provider} />)
+                    <Provider 
+                        key={provider.provider_id} 
+                        provider={provider} 
+                        toggle={toggle}
+                        watchList={watchList}
+                    />)
                 }
             </div>
         </div>
--- src/pages/Providers.jsx
+++ src/pages/Providers.jsx
@@ -5,16 +5,11 @@ import { getProviders } from "../services/tmdb-api";
 const ProvidersPage = ({watchList, toggle}) => {
     const [providers, setProviders] = useState(null);
     
-
-    // Call popular movies based on the provider that is passed in
     useEffect(() => {
         getProviders().then((providers) => setProviders(providers))
-        //console.log(providers)
     }, [])
-    //console.log(providers)
 
     return ( 
-        // <h1>this works</h1>
         <ProviderList providers={providers} watchList={watchList} toggle={toggle} />
      );
 }
